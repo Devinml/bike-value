b8c8a9b7254ca2c49bfff42b45a190864ae9e74c
Devinml <devinlink8@gmail.com>
2020-05-06 16:56:31 -0600

    added writing to a csv file

--- webscrapingpinkbike.py
+++ webscrapingpinkbike.py
@@ -2,20 +2,22 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from time import sleep
 import csv
+import string
 
-def remove_commas(string):
-    exclude = ','
-    return ''.join(ch for ch in string if ch not in exclude)
-print('hello,there ,,, !')
+def remove_punc(string_):
+    exclude = set(string.punctuation)
+    s =  ''.join(ch for ch in string_ if ch not in exclude)
+    return s
+
 
 MAX_PAGE_NUM = 1
 
 driver = webdriver.Firefox()
 
-'''
+
 with open('results.csv', 'w') as f:
-    f.write("Number of photos, Year and Model , Category, Condition, Material, Frame Size, Wheel Size, Front Travel, Rear Travel, Location,State/Province,Country, Seller, Price \n")
-'''
+    f.write("Title,Location,Condition,Material,Frame Size,Wheel Size,Front Travel,Rear Travel,Price,City,State/Prov,Country\n")
+
 for i in range(1, MAX_PAGE_NUM + 1):
      
     url = f"https://www.pinkbike.com/buysell/list/?region=3&page={i}&category=2"
@@ -30,8 +32,11 @@ for i in range(1, MAX_PAGE_NUM + 1):
        
     for i in range(4):#range(20):
         elems = driver.find_elements_by_xpath("(//b[contains(.,'[Read More]')])")
+        print(len(elems))
         elems[i].click()
         try:
+            
+            title = driver.find_element_by_css_selector('.hboxr-c3-h > h1:nth-child(1)')
             location = driver.find_element_by_css_selector('.hbox-c3-m > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2)')
             condition = driver.find_element_by_css_selector('.hbox-c3-m > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(7) > td:nth-child(2)')
             frame_material = driver.find_element_by_css_selector('.hbox-c3-m > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(8) > td:nth-child(2)')
@@ -40,45 +45,26 @@ for i in range(1, MAX_PAGE_NUM + 1):
             front_travel = driver.find_element_by_css_selector('.hbox-c3-m > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(11) > td:nth-child(2)')
             rear_travel = driver.find_element_by_css_selector('.hbox-c3-m > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(12) > td:nth-child(2)')
             price  = driver.find_element_by_css_selector('.hbox-c3-m > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(13) > td:nth-child(2)')
+            location = driver.find_element_by_css_selector('.hbox-c3-m > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2)')
             description = driver.find_element_by_css_selector('.hbox-c3-m > div:nth-child(1) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1)')
+            print(title.text.strip())
+            print(frame_material.text.strip())
+            print(condition.text.strip())
+            print(frame_size.text)
+            print(wheel_size.text)
+            print(front_travel.text)
+            print(rear_travel.text)        
+            print(price.text)
+            print(location.text)
+            print(remove_punc(description.text.strip()))
+            
+            with open('results.csv', 'a') as f:
+                f.write(title.text.strip() + ',' + location.text.strip() + ',' + condition.text.strip() + ',' + frame_material.text.strip()+','+frame_size.text.strip()+','+wheel_size.text.strip()+','+front_travel.text.strip()+','+rear_travel.text.strip()+','+price.text.strip()+','+location.text()+','+remove_punc(description.text.strip())+'\n')
+
         except :
-            continue
-        
-        print(frame_material.text)
-        print(condition.text.strip())
-        print(frame_size.text)
-        print(wheel_size.text)
-        print(front_travel.text)
-        print(rear_travel.text)        
-        print(price.text)
-        print(description.text)
+            driver.back()
         driver.back()
 
-       
-
-
-
-
-        
-    
-    for i in range(len(titles)):
-        pass
-        #lems = driver.find_element_by_xpath("(//b[contains(.,'[Read More]')])")
-        #print(elems)
-        #for elem in elems:
-         #   print(elem.get_attribute('href'))
-        #print(type(titles[i]))   
-        #read_more_link[i].click()
-        #print('Clicked')
-        #driver.back()        
-        
-
-    #titles = driver.find_element_by_css_selector('#csid2750780')
-    #prices = driver.find_elements_by_xpath('//span[@class="item-price"]')
-    #print(title)
-    #print(title[19].text)
-    
-    
     '''
     num_page_items = len(titles)
     with open('results.csv', 'a') as f:
