5573835a1862600cc6c24f46821eace07b0218fa
Devinml <devinlink8@gmail.com>
2020-05-07 19:39:34 -0600

    added a delay to remove race conflict

--- webscrapingpinkbike.py
+++ webscrapingpinkbike.py
@@ -24,7 +24,7 @@ driver = webdriver.Firefox(firefox_profile=profile)
 #with open('results.csv', 'w') as f:
     #f.write("Title,Location,Condition,Material,Frame Size,Wheel Size,Front Travel,Rear Travel,Price,City,State/Prov,Country\n")
 f = open('results.csv','w')
-f.write("Title,Condition,Material,Frame Size,Wheel Size,Front Travel,Rear Travel,Price,City,State/Prov,Country,Description\n")
+f.write("Title,Condition,Material,Frame Size,Wheel Size,Front Travel,Rear Travel,Price,City,State/Prov,Country,Description,Date Posted\n")
 #with open('results.csv', 'a+') as f:
 for i in range(1, MAX_PAGE_NUM + 1):
      
@@ -47,10 +47,13 @@ for i in range(1, MAX_PAGE_NUM + 1):
         '.hbox-c3-m > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(12) > td:nth-child(2)', # Rear Travel
         '.hbox-c3-m > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(13) > td:nth-child(2)', # Price
         '.hbox-c3-m > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2)', # Location
-        '.hbox-c3-m > div:nth-child(1) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1)'  # Description 
-    ]
+        '.hbox-c3-m > div:nth-child(1) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1)',  # Description 
+        '.hbox-c3-m > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(2)' # Date
 
-    for i in range(2):#range(20):
+    ]
+    if i %10 == 0:
+        sleep(60)
+    for i in range(20):#range(20):
         
         elems = driver.find_elements_by_xpath("(//b[contains(.,'[Read More]')])")
         elems[i].click()
@@ -63,6 +66,8 @@ for i in range(1, MAX_PAGE_NUM + 1):
                 elif key == 9:
                     bike_data += remove_punc(driver.find_element_by_css_selector(selector).text) + ','
                     bike_data.replace('\n','')
+                elif key == 10:
+                    bike_data += driver.find_element_by_css_selector(selector).text + ','
                 else:
                     bike_data += remove_punc(driver.find_element_by_css_selector(selector).text) + ','
                     bike_data.replace('\n','')
